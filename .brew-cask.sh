941e1fba9d99ca786ee59b34b499ecaa5d1f20d7
Adam Stitzer <astitzer@gmail.com>
2015-02-21 22:07:07 -0500

    aliases and brew-cask

---
+++ .brew-cask.sh
@@ -0,0 +1,13 @@
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# dev 
+brew cask install sublime-text3
+
+# daily
+brew cask install citrix-receiver
+
+# browsers
+brew cask install google-chrome
