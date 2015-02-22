2c6b7fbf2a2424f3725ce3ba5ccde662b5391641
Adam Stitzer <astitzer@gmail.com>
2015-02-22 08:56:36 -0500

    brew-cask changes

--- brew-cask.sh
+++ brew-cask.sh
@@ -5,9 +5,20 @@ brew tap caskroom/versions
 
 # dev 
 brew cask install sublime-text3
+brew cask install github
 
 # daily
-brew cask install citrix-receiver
+brew cask install dropbox
 
 # browsers
 brew cask install google-chrome
+brew cask install google-hangouts
+brew cask install firefox-nightly --force
+
+# less often
+brew cask install virtualbox
+brew cask install citrix-receiver
+brew cask install vlc
+
+#brew install npm
+#npm install grunt-cli -g
