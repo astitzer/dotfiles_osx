f99e5720e3c612ebbf4ac1135ee73ebbe8ba16d8
Adam Stitzer <astitzer@gmail.com>
2017-02-13 15:11:01 -0500

    avs - update brew-cask for sublime-text

--- brew-cask.sh
+++ brew-cask.sh
@@ -4,7 +4,7 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 # dev 
-brew cask install sublime-text3
+brew cask install sublime-text
 brew cask install github
 brew cask install macports
 brew cask install transmit
